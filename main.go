815f02e1f60d4e249c5f242878c540ee16c680a6
Simon Carr <simon@soxprox.com>
2023-09-29 23:04:28 +0100

    monitor cpu usage cross platform

--- main.go
+++ main.go
@@ -3,8 +3,16 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"time"
+
+	"github.com/shirou/gopsutil/cpu"
 )
 
+func dealWithErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
 func main() {
 	os := runtime.GOOS
@@ -15,6 +23,7 @@ func main() {
 	} else {
 		fmt.Println("OS not supported")
 	}
+	crossPlatform()
 }
 
 func linux() {
@@ -25,5 +34,13 @@ func windows() {
 	fmt.Println("Windows")
 }
 
-
-
+func crossPlatform() {
+	for {
+		percentage, err := cpu.Percent(0, true)
+		dealWithErr(err)
+		for idx, percent := range percentage {
+			fmt.Printf("Core %d: %f\n", idx, percent)
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
